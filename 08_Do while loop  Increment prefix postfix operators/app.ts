2c345d14894febf19f3cb7867a9ca81e256da1b9
moiz-xyz <a.moizniaxi123@gmail.com>
2025-03-21 23:50:45 +0500

    Prefix postfix operators

---
+++ 08_Do while loop  Increment prefix postfix operators/app.ts
@@ -0,0 +1,13 @@
+//  Prefix postfix operators
+
+// Post fix increment operator ( variable ++)
+let index : number = 1
+let result : number = index ++
+console.log( "index",index); // 2 
+console.log("reuslt", result); //1
+
+// Pre Fix operator (++variable)
+let index2 : number = 1
+let result2 : number = ++index 
+console.log( "index",index2); // 1
+console.log("reuslt", result2); //3
